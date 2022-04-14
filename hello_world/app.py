7ec8670344dc48b70ff36b9fe2a463fd6abf5b33
cuongnm <cuonghp92@gmail.com>
2022-04-15 00:02:25 +0700

    hello giap

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello Giap",
             # "location": ip.text.replace("\n", "")
         }),
     }
